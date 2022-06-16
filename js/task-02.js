bf5f49551563774aae8e9433cb6e3d4ddb16c7e5
Oksana-Dyka15888 <kseniadika2210@gmail.com>
2022-06-16 17:20:15 +0300

    task-two

--- js/task-02.js
+++ js/task-02.js
@@ -6,4 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
- 
+
+
+
+const ulItemEl = document.querySelector(`ul`)
+console.log(ulItemEl)
+const Elements = ingredients.map((ingredients) => ` <li class= "item">${ingredients} </li>`).join("");
+console.log(Elements);
+ulItemEl.innerHTML = Elements;
+
+
